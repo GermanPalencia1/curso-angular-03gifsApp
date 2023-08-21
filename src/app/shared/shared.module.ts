e7fb49fbe0204fe5c2fb1aa3d0762cda4907a7f1
Germán <german.palencia@adentia.es>
2023-08-21 14:45:04 +0200

    Create sidebar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent
+  ],
   imports: [
     CommonModule
+  ],
+  //Acordarse de exportarlo para poder usarlo en el módulo principal
+  exports: [
+    SidebarComponent
   ]
 })
 export class SharedModule { }
