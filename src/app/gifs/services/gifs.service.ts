efb1c97833d48011dfc7d7ca8b5c21a35c97ff59
Germán <german.palencia@adentia.es>
2023-08-23 13:54:25 +0200

    Gifs title when search

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -58,7 +58,6 @@ export class GifsService {
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
       .subscribe(resp => {
         this.gifsList = resp.data;
-        console.log({gifs: this.gifsList})
       });
   }
 
