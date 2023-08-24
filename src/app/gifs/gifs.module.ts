2c735f70fcd7a2a7921914cbaf1582b109bf7b23
Germán <german.palencia@adentia.es>
2023-08-24 13:43:44 +0200

    Lazy Image component created

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import {  HomePageComponent } from './pages/home/home-page.component';
@@ -15,7 +17,8 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     SearchBoxComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
+    LazyImageComponent,
     SidebarComponent
   ],
   imports: [
@@ -13,6 +16,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   ],
   //Acordarse de exportarlo para poder usarlo en el módulo principal
   exports: [
+    LazyImageComponent,
     SidebarComponent
   ]
 })
