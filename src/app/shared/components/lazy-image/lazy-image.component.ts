0428ebc71d805c0b0f5d154f7f956e02af1e60ba
Germán <german.palencia@adentia.es>
2023-08-24 14:06:27 +0200

    Show spinner while image is loading

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html'
 })
 export class LazyImageComponent {
+
+  public hasLoaded: boolean = false;
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  onLoad(): void {
+    console.log('Image loaded');
+    this.hasLoaded = true;
+  }
 
 }
