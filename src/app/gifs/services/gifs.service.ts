6ba4a28debcf9ee6302786f0ef9e58839c8e74b6
Germán <german.palencia@adentia.es>
2023-08-24 12:49:40 +0200

    Read and show info saved on LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,8 +13,15 @@ export class GifsService {
   private _tagsHistory: string[] = [];
   private apiKey: string ='nb2gAcKsvyK2v7msXZpxKY6FOEn6GKND';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
+  localStorage: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    /*
+    Cuando el servicio sea iyectado por primera vez se ve si hay algo en el almacenamiento
+    del LocalStorage para leerlo y sacar la info.
+    */
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -45,6 +52,22 @@ export class GifsService {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory))
   }
 
+  //Leer info del localStorage
+  private loadLocalStorage(): void {
+
+    //Si no hay datos -> no hacer nada
+    if(!localStorage.getItem('history')) return;
+
+    //Si hay datos (indicar que no es null):
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    //Hacer validación para ver si el historial es 0
+    if (this._tagsHistory.length === 0) return;
+
+    //Sino buscamos el primero para mostrarlo al recargar o abrir de nuevo
+    this.searchTag(this._tagsHistory[0]);
+  }
+
   public searchTag(tag: string): void {
 
     if(tag.length === 0) return;
@@ -52,7 +75,7 @@ export class GifsService {
     this.organizeHistory(tag);
 
     //Hacer petición a la Api para sacar datos -> Se saca por el app.module mejor
-    //Luego el private de arriva con el http
+    //Luego el private de arriba con el http
     //Luego lo que hay debajo de esto.
     // fetch('http://api.giphy.com/v1/gifs/search?api_key=nb2gAcKsvyK2v7msXZpxKY6FOEn6GKND&q=valorant&limit=10')
     //   .then(resp => resp.json)
